92ae3c438d4caaa9b47c17e1af75fc14ac7618a5
zarghamaijaz <zaijaz@vaporvm.com>
2025-01-14 13:04:21 +0500

    Fixed the homepage issue. Or maybe not

--- pages/index.js
+++ pages/index.js
@@ -57,7 +57,7 @@ const Home = ({ posts }) => {
 
 
 export async function getServerSideProps() {
-    const { data } = await axios.get('/posts');
+    const { data } = await axios.get(`${process.env.NEXT_PUBLIC_API}/posts`);
     // console.log(data);
     return {
         props: {posts: data},
